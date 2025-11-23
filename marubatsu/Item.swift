45eb2581a67eddc77810a96d6cf63248748e8908
tsukasa <soudesu5566@gmail.com>
2025-11-24 00:57:46 +0900

    Initial Commit

---
+++ marubatsu/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  marubatsu
+//
+//  Created by tsukasa on 2025/11/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ marubatsuTests/marubatsuTests.swift
@@ -0,0 +1,17 @@
+//
+//  marubatsuTests.swift
+//  marubatsuTests
+//
+//  Created by tsukasa on 2025/11/24.
+//
+
+import Testing
+@testable import marubatsu
+
+struct marubatsuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
